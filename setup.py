fffa7d43f21a2ddee884df59ccbb89f283192c62
luiztauffer <luiz_tauffer@hotmail.com>
2020-02-13 17:31:29 +0700

    requirements

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['PySide2', 'voila']
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Luiz Tauffer",
