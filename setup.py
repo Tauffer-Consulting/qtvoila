586d104092193a877e0a8be2dbded0cc59c34aa0
luiz <luiz_tauffer@hotmail.com>
2022-10-18 09:35:35 +0200

    update version

--- setup.py
+++ setup.py
@@ -16,13 +16,9 @@ setup(
     author_email='luiz@taufferconsulting.com',
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
@@ -39,6 +35,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/luiztauffer/qtvoila',
-    version='1.0.0',
+    version='2.0.0',
     zip_safe=False,
 )
